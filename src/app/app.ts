c880ff986010a792d517698e3ce72d4255320de8
GeorgePaidis <paidisg@hotmail.gr>
2025-11-19 16:05:53 +0200

    add: list, navbar, welcome components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Step2PersonTable } from './components/step2-person-table/step2-person-table';
 import { Step3PersonTableInput } from './components/step3-person-table-input/step3-person-table-input';
-import { Person } from './shared/interfaces/person';
 import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
 import { Step5EventBind } from './components/step5-event-bind/step5-event-bind';
 
+import { Navbar } from './components/navbar/navbar';
+import { ListGroupMenu } from './components/list-group-menu/list-group-menu';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Person } from './shared/interfaces/person';
 @Component({
   selector: 'app-root',
-  imports: [Step2PersonTable, Step3PersonTableInput, Step4ForDirective, Step5EventBind],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    Navbar,
+    ListGroupMenu,
+    Step2PersonTable,
+    Step3PersonTableInput,
+    Step4ForDirective,
+    Step5EventBind
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
